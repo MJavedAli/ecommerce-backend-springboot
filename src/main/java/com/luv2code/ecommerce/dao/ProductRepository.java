67b5f699d6b1906e2691b142030ed5956727ad80
jali <javed.ali@softwareag.com>
2022-09-19 15:09:45 +0530

    patch 2

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,16 +1,17 @@
 package com.luv2code.ecommerce.dao;
 
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import com.luv2code.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long>{
 
-    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+	
 }
